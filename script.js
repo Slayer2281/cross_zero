91ed141305ebe30356d253f3dadb31daf5756762
Farida15 <>
2021-06-18 18:26:21 +0700

    пошаговость

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+let icon = "icon-x"
+let map = new Array(9)
 function move(id){
-    this.className = "cell " + "icon-o";
+if(map[id])
+  return;
+   document.getElementById(id).className = "cell "+ icon
+   map[id] = icon.slice(-1)
+   document.getElementById(map[id]).className = ""
+   if(icon == "icon-x")
+   {
+       icon = "icon-o"
+       document.getElementById("o").className = "turn"
+   }else
+   {
+       icon = "icon-x"
+       document.getElementById("x").className = "turn"
+   }
 }
